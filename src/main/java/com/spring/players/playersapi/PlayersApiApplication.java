ad051e793353619f20c40c782f1c6f28a9237c1d
Muhannad-abu-jaish <mohnad20211@gmail.com>
2023-05-20 22:58:52 +0300

    Project init

---
+++ src/main/java/com/spring/players/playersapi/PlayersApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.players.playersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/players/playersapi/PlayersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.players.playersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
